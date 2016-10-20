523b86e799fe0385f895c1f0e69c4a586dc4b045
Matt Ickstadt <mattico8@gmail.com>
2016-10-19 21:23:10 -0500

    Attempt to memmap the image buffers

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+- Memmap decoded image buffers, not source files
+- Allow restricting the number of images to use to reduce memory use
+- Make gui/bundle with ffmpeg for normies
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 extern crate clap;
 extern crate image;
 extern crate memmap;
@@ -6,8 +5,10 @@ extern crate memmap;
 use std::path::{PathBuf, Path};
 use std::process::exit;
 use std::f64::consts::PI;
+use std::fs::File;
+use std::io::prelude::*;
 
-use image::{GenericImage, DynamicImage, ImageFormat, RgbImage, ImageBuffer};
+use image::{Rgb, DynamicImage, ImageFormat, RgbImage, ImageBuffer};
 use memmap::{Mmap, Protection};
 
 const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -26,7 +27,7 @@ fn main() {
             .required(true))
         .get_matches();
 
-    let assert_dir = |d: &PathBuf| {
+    let assert_is_dir = |d: &PathBuf| {
         if !d.is_dir() {
             println!("Argument must be directory");
             exit(0);
@@ -35,7 +36,7 @@ fn main() {
 
     let input_files: Vec<PathBuf> = if let Some(dir) = matches.value_of("INPUT") {
         let d = PathBuf::from(dir);
-        assert_dir(&d);
+        assert_is_dir(&d);
         d.read_dir().unwrap().map(|x| x.unwrap().path()).collect()
     } else {
         unreachable!();
@@ -43,7 +44,7 @@ fn main() {
 
     let out_dir: PathBuf = if let Some(dir) = matches.value_of("OUTPUT") {
         let d = PathBuf::from(dir);
-        assert_dir(&d);
+        assert_is_dir(&d);
         d
     } else {
         unreachable!();
@@ -57,17 +58,29 @@ fn main() {
     }
 }
 
+fn mmap_image_buffer(file: &PathBuf) -> Option<ImageBuffer<Rgb<u8>, &[u8]>> {
+    let rgb_image = match image::open(file) {
+        Ok(img) => img.to_rgb(),
+        Err(_) => {
+            println!("Unable to open file as image, ignoring: '{}'", file.to_string_lossy());
+            return None;
+        },
+    };
+    let width = rgb_image.width();
+    let height = rgb_image.height();
+
+    let mut mmap_file_path = file.clone();
+    mmap_file_path.set_extension("bin");
+    let mut mmap_file = File::create(mmap_file_path).unwrap();
+    mmap_file.write_all(&*rgb_image.into_raw()).unwrap();
+    let mmap = Mmap::open(&mmap_file, Protection::Read).unwrap();
+    forget(mmap);
+
+    ImageBuffer::from_raw(width, height, unsafe{mmap.as_slice()})
+}
+
 fn spiralize(frames: &Vec<PathBuf>, out_dir: &PathBuf) {   
-    let frames: Vec<RgbImage> = frames.iter().filter_map(|f| {
-        let mmap = Mmap::open_path(f, Protection::Read).unwrap();
-        match image::load_from_memory(unsafe { mmap.as_slice() }) {
-            Ok(img) => Some(img.to_rgb()),
-            Err(_) => {
-                println!("Unable to open file as image, ignoring: '{}'", f.to_string_lossy());
-                None
-            },
-        }
-    }).collect();
+    let frames: Vec<ImageBuffer<Rgb<u8>, &[u8]>> = frames.iter().filter_map(mmap_image_buffer).collect();
 
     let width = frames[0].width();
     let height = frames[0].height();
