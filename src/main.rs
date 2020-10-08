4e37008b5da2be7e6871a43f4a3ed2d81a756dec
Matt Ickstadt <mattico8@gmail.com>
2020-10-07 23:48:38 -0500

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::exit;
 
-use image::{Rgb, RgbImage, ImageBuffer, Pixel};
+use image::{ImageBuffer, Pixel, Rgb, RgbImage};
 use memmap::Mmap;
 use tempfile::TempDir;
 
@@ -22,19 +22,28 @@ fn main() {
         .version(APP_VERSION)
         .author(APP_AUTHORS)
         .about("Helps create a swirly timelapse gif")
-        .after_help("Supported input formats: PNG JPG GIF ICO BMP\n\
-                     Output images will be saved in PNG format.")
-        .arg(clap::Arg::with_name("INPUT")
-            .help("A directory full of images to use")
-            .required(true))
-        .arg(clap::Arg::with_name("OUTPUT")
-            .help("A directory to output the images to")
-            .required(true))
+        .after_help(
+            "Supported input formats: PNG JPG GIF ICO BMP\n\
+                     Output images will be saved in PNG format.",
+        )
+        .arg(
+            clap::Arg::with_name("INPUT")
+                .help("A directory full of images to use")
+                .required(true),
+        )
+        .arg(
+            clap::Arg::with_name("OUTPUT")
+                .help("A directory to output the images to")
+                .required(true),
+        )
         .get_matches();
 
     let assert_is_dir = |d: &PathBuf| {
         if !d.is_dir() {
-            println!("ERROR: Arguments must be directories.\n{} is not a directory", d.to_string_lossy());
+            println!(
+                "ERROR: Arguments must be directories.\n{} is not a directory",
+                d.to_string_lossy()
+            );
             exit(0);
         }
     };
@@ -62,7 +71,7 @@ fn main() {
     }
 }
 
-fn spiralize(input_files: &Vec<PathBuf>, out_dir: &PathBuf) {   
+fn spiralize(input_files: &Vec<PathBuf>, out_dir: &PathBuf) {
     let mut width = 0;
     let mut height = 0;
     let temp_dir = TempDir::new_in("spiralizer").unwrap();
@@ -71,30 +80,37 @@ fn spiralize(input_files: &Vec<PathBuf>, out_dir: &PathBuf) {
 
     let mut load_progress_bar = pbr::ProgressBar::new(input_files.len() as u64);
 
-    let mmaps: Vec<Mmap> = input_files.iter().filter_map(|file| {
-        let rgb_image = match image::open(file) {
-            Ok(img) => img.to_rgb(),
-            Err(_) => return None,
-        };
-        if width == 0 && height == 0 {
-            width = rgb_image.width();
-            height = rgb_image.height();
-        } else if width != rgb_image.width() || height != rgb_image.height() {
-            println!("ERROR: Images must all be the same size.");
-            exit(0);
-        }
-        let mut mmap_file_path = temp_dir.path().join(file.file_name().unwrap());
-        mmap_file_path.set_extension("bin");
-        let mut mmap_file = File::create(&mmap_file_path).unwrap();
-        mmap_file.write_all(&*rgb_image.into_raw()).unwrap();
-        load_progress_bar.inc();
-        Some(unsafe { Mmap::map(&mmap_file).unwrap() })
-    }).collect();
+    let mmaps: Vec<Mmap> = input_files
+        .iter()
+        .filter_map(|file| {
+            let rgb_image = match image::open(file) {
+                Ok(img) => img.to_rgb(),
+                Err(_) => return None,
+            };
+            if width == 0 && height == 0 {
+                width = rgb_image.width();
+                height = rgb_image.height();
+            } else if width != rgb_image.width() || height != rgb_image.height() {
+                println!("ERROR: Images must all be the same size.");
+                exit(0);
+            }
+            let mut mmap_file_path = temp_dir.path().join(file.file_name().unwrap());
+            mmap_file_path.set_extension("bin");
+            let mut mmap_file = File::create(&mmap_file_path).unwrap();
+            mmap_file.write_all(&*rgb_image.into_raw()).unwrap();
+            load_progress_bar.inc();
+            Some(unsafe { Mmap::map(&mmap_file).unwrap() })
+        })
+        .collect();
 
-    load_progress_bar.finish_print(
-        &format!("Loaded {} images. {} files ignored.\n", mmaps.len(), input_files.len() - mmaps.len()));
+    load_progress_bar.finish_print(&format!(
+        "Loaded {} images. {} files ignored.\n",
+        mmaps.len(),
+        input_files.len() - mmaps.len()
+    ));
 
-    let frames: Vec<ImageBuffer<Rgb<u8>, &[u8]>> = mmaps.iter()
+    let frames: Vec<ImageBuffer<Rgb<u8>, &[u8]>> = mmaps
+        .iter()
         .filter_map(|m| ImageBuffer::from_raw(width, height, m.as_ref()))
         .collect();
 
@@ -106,8 +122,8 @@ fn spiralize(input_files: &Vec<PathBuf>, out_dir: &PathBuf) {
     for i in 0..frames.len() {
         for (x, y, pixel) in output.enumerate_pixels_mut() {
             let two_pi = 2f64 * PI;
-            let c_x = (height/2) as i32 - y as i32;
-            let c_y = (width/2) as i32 - x as i32;
+            let c_x = (height / 2) as i32 - y as i32;
+            let c_y = (width / 2) as i32 - x as i32;
             let mut pixel_angle = f64::atan2(c_x as f64, c_y as f64);
             if pixel_angle < 0.0 {
                 pixel_angle += two_pi;
@@ -127,22 +143,34 @@ fn spiralize(input_files: &Vec<PathBuf>, out_dir: &PathBuf) {
             if blend_source_frame < 0 || blend_source_frame >= frames.len() as isize {
                 *pixel = *frames[source_frame as usize].get_pixel(x, y);
                 continue;
-            } 
+            }
 
             let blend_val = (source_frame_frac - source_frame_round).abs();
 
             let mut main_pixel = frames[source_frame as usize].get_pixel(x, y).to_rgba();
-            let mut blend_pixel = frames[blend_source_frame as usize].get_pixel(x, y).to_rgba();
+            let mut blend_pixel = frames[blend_source_frame as usize]
+                .get_pixel(x, y)
+                .to_rgba();
 
             blend_pixel[3] = (blend_val * 256.0) as u8;
             main_pixel.blend(&blend_pixel);
 
             *pixel = main_pixel.to_rgb();
         }
-        output.save(out_dir.join(format!("frame_{:04}.png", i)).to_str().unwrap()).unwrap();
+        output
+            .save(
+                out_dir
+                    .join(format!("frame_{:04}.png", i))
+                    .to_str()
+                    .unwrap(),
+            )
+            .unwrap();
         save_progress_bar.inc();
     }
 
-    save_progress_bar.finish_print(
-        &format!("Saved {} images to {}\n", frames.len(), out_dir.to_string_lossy()));
+    save_progress_bar.finish_print(&format!(
+        "Saved {} images to {}\n",
+        frames.len(),
+        out_dir.to_string_lossy()
+    ));
 }
